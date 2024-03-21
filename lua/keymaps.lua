d59a94376ce986ce8059a2446a26053d5b9b2469
S1ro1 <matej.sirovatka@gmail.com>
2024-03-21 02:41:25 +0100

    New keymaps, gruvbox and copilot

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,19 +1,17 @@
 vim.g.mapleader = " "
 
-local default_opts = {silent=true}
+local default_opts = { silent = true }
 -- TELESCOPE
-vim.keymap.set('n', '<Char-0xAA>', ':Telescope find_files<CR>', default_opts) -- p
-vim.keymap.set('n', '<Char-0xAB>', ':Telescope live_grep<CR>', default_opts) -- f
-vim.keymap.set('n', '<Char-0xAC>', ':Telescope buffers<CR>', default_opts) -- b
+vim.keymap.set('n', '<C-p>', ':Telescope find_files<CR>', default_opts) -- p
+vim.keymap.set('n', '<C-f>', ':Telescope live_grep<CR>', default_opts)  -- f
+vim.keymap.set('n', '<C-b>', ':Telescope buffers<CR>', default_opts)    -- b
 vim.keymap.set('n', '<leader>h', ':Telescope help_tags<CR>', default_opts)
 
 --NEOTREE
 vim.keymap.set('n', '<C-e>', ':Neotree toggle<CR>', default_opts) -- this is remapped inside alacritty and tmux
 
 -- LSP
-vim.keymap.set('n', '<Char-0xAE>', vim.lsp.buf.hover, default_opts)
 vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, default_opts)
 vim.keymap.set('n', 'gd', vim.lsp.buf.declaration, default_opts)
 vim.keymap.set('n', 'gD', vim.lsp.buf.definition, default_opts)
 vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, default_opts)
-
---
+++ lua/plugins/gruvbox.lua
@@ -0,0 +1,8 @@
+return {
+  'sainnhe/gruvbox-material',
+  priority = 10,
+  config = function()
+    vim.cmd('colorscheme gruvbox-material')
+    vim.g.gruvbox_material_background = 'hard'
+  end
+}
